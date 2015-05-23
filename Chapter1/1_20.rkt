e28665ef2ce284b6bdcf6d0ab4d9f27935002363
HelainSchoonjans <helain90@hotmail.com>
2015-05-23 13:25:39 +0200

    Exercises 20, 21, 22.

--- Chapter1/1_20.rkt
+++ Chapter1/1_20.rkt
@@ -1,1 +1,70 @@
 #lang racket
+(define (test condition x y)
+  (if condition
+      x
+      y))
+
+(define (rem a b)
+  (if (= b 0)
+      0
+      (remainder a b)))
+
+(define (gcd-illus-normal a b)
+  (displayln (string-append ";(gcd " (number->string a) " " (number->string b) ")"))
+  (test (< b 1)
+      a
+      (gcd-illus-normal b (rem a b))))
+
+(define (gcd-illus-applicative a b)
+  (displayln (string-append ";(gcd " (number->string a) " " (number->string b) ")"))
+  (if (= b 0)
+      a
+      (gcd-illus-applicative b (remainder a b))))
+
+; a) illustrate process generated for evaluating (gcd 206 40) in normal order
+;(gcd-illus-normal 206 40)
+;(gcd 206 40)
+;(gcd 40 6)
+;(gcd 6 4)
+;(gcd 4 2)
+;(gcd 2 0)
+;(gcd 0 0)
+;(gcd 0 0)
+;(gcd 0 0)
+;(gcd 0 0)
+;(gcd 0 0)
+;(gcd 0 0)
+;(gcd 0 0)
+;(gcd 0 0)
+;(gcd 0 0)
+;(gcd 0 0)
+;(gcd 0 0)
+; ...
+
+; b) indicate the remainder operations performed
+;(remainder 206 40)
+;(remainder 40 6)
+;(remainder 6 4)
+;(remainder 4 2)
+; i made a function to avoid calling remainder with a second argument equal to zero
+
+; c) how many remainder operations are performed?
+; from a), 4 remainder performed
+
+; how many operations are performed in the applicative order of evaluation?
+(gcd-illus-applicative 206 40)
+;(gcd 206 40)
+;(gcd 40 6)
+;(gcd 6 4)
+;(gcd 4 2)
+;(gcd 2 0)
+;2
+
+; b) indicate the remainder operations performed
+;(remainder 206 40)
+;(remainder 40 6)
+;(remainder 6 4)
+;(remainder 4 2)
+
+; c) how many remainder operations are performed?
+; from a), 4 remainder performed (the last gdc step doesn't call remainder)
--- Chapter1/1_22.rkt
+++ Chapter1/1_22.rkt
@@ -1,1 +1,67 @@
-#lang racket
+; to continue
+#lang planet neil/sicp
+(define (square x)
+  (* x x))
+(define (smallest-divisor n)
+  (find-divisor n 2))
+(define (find-divisor n test-divisor)
+  (cond ((> (square test-divisor) n) n)
+        ((divides? test-divisor n) test-divisor)
+        (else (find-divisor n (+ test-divisor 1)))))
+(define (divides? a b)
+  (= (remainder b a) 0))
+(define (prime? n)
+  (= n (smallest-divisor n)))
+(define (timed-prime-test n)
+  (newline)
+  (start-prime-test n (runtime)))
+(define (start-prime-test n start-time)
+  (if (prime? n)
+      (report-prime (- (runtime) start-time))))
+(define (report-prime elapsed-time)
+  (display " *** ")
+  (display elapsed-time))
+(define (do x y)
+  (if #f
+      x
+      y))
+
+; Using this procedure, write a procedure search-for-primes that checks the primality of
+; consecutive odd integers in a specified range. 
+(define (search-for-primes start end)
+  (if (= end start)
+      (display "")
+      (do (timed-prime-test start) (search-for-primes (+ 1 start) end))))
+
+; Use your procedure to find the three smallest primes larger than 1000; larger than 10,000;
+;larger than 100,000; larger than 1,000,000.
+(search-for-primes 1000 1020)
+;1009 *** 0
+;1013 *** 0
+;1019 *** 0
+(search-for-primes 10000 10040)
+;10007 *** 0
+;10009 *** 0
+;10037 *** 1000
+(search-for-primes 100000 100050)
+;100003 *** 0
+;100019 *** 0
+;100043 *** 0
+(search-for-primes 1000000 1000060)
+;1000003 *** 0
+;1000033 *** 1000
+;1000037 *** 0
+
+; Note the time needed to test each prime. 
+; done above
+
+; Since the testing algorithm has order of growth of ( n), you should expect that testing 
+; for primes around 10,000 should take about 10 times as long as testing for primes around
+; 1000. 
+; Do your timing data bear this out? How well do the data for 100,000 and 1,000,000 support the
+; n prediction? 
+; no
+
+;Is your result compatible with the notion that programs on your machine run in time
+; proportional to the number of steps required for the computation?
+; no
