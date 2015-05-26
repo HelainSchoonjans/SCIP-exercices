ebe960fcfd07eaaf5e5895406e1b5dd855016070
HelainSchoonjans <helain90@hotmail.com>
2015-05-26 22:22:05 +0200

    Exercises.

--- Chapter2/2_12.rkt
+++ Chapter2/2_12.rkt
@@ -1,1 +1,19 @@
 #lang racket
+
+(define (make-interval a b) (cons a b))
+(define (lower-bound interval)
+  (min (car interval) (cdr interval)))
+(define (upper-bound interval)
+  (max (car interval) (cdr interval)))
+(define (make-center-percent c tolerance)
+  (make-interval (- c (* c (/ tolerance 100))) (+ c (* c (/ tolerance 100)))))
+(define (center i)
+  (/ (+ (lower-bound i) (upper-bound i)) 2))
+(define (width i)
+  (/ (- (upper-bound i) (lower-bound i)) 2))
+; uncertainty = width / midpoint
+; must return a percentage
+(define (percent interval)
+  (* 100 (/ (width interval) (center interval))))
+
+(percent (make-center-percent 10 10))
--- Chapter2/2_13.rkt
+++ Chapter2/2_13.rkt
@@ -1,1 +1,7 @@
+; to continue
 #lang racket
+; let's compute the upper limit :
+; (R1 + R1*tolerance1/100)*(R2 + R2 * tolerance2/100)
+; = R1*R2*(100 + tolerance1)*(100 + tolerance2)/(100*100)
+; = (R1*R2)*(100 + tolerance1)*(100 + tolerance2)/(100*100)
+; we can say that the new tolerance is (100 + tolerance1)*(100 + tolerance2)/(100*100)
