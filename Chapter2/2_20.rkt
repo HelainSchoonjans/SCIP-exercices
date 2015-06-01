6fc4b938d580324ac27a18e36ba1962078a257e7
HelainSchoonjans <helain90@hotmail.com>
2015-06-01 23:00:49 +0200

    Exercise 20

--- Chapter2/2_20.rkt
+++ Chapter2/2_20.rkt
@@ -1,1 +1,16 @@
-#lang racket
+; to continue
+#lang planet neil/sicp
+(define (same-parity x . y)
+  (define (same-parity-rec x y)
+    (display (car y))
+    (display (cdr y))
+    (display (remainder x 2))
+    (display (remainder (car y) 2))
+    (cond ((null? y) x)
+          ((= (remainder x 2) (remainder (car y) 2)) (cons (car y) (same-parity x (cdr y))))
+          (else (same-parity x (cdr y)))))
+  (same-parity-rec x y))
+
+
+(same-parity 1 2 3 4 5 6 7)
+(same-parity 2 3 4 5 6 7)
